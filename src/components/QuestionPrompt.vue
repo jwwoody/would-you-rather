41cf6bd089b75e65a74734ffe72bb46de40494e0
Jeffrey Woodbury <jeffrey.woodbury@byu.edu>
2023-11-10 17:25:31 -0700

    created the app to be functioning

---
+++ src/components/QuestionPrompt.vue
@@ -0,0 +1,152 @@
+// components/QuestionPrompt.vue
+<template>
+  <div class="site-container">
+    <button @click="fetchQuestion" v-if="!loading" class="btn-show-question">Show Question</button>
+    <div v-if="loading" class="loading">Loading...</div>
+    <div v-if="question" class="question-box">
+      <div class="question-image-container">
+        <img v-if="question.imageUrl" :src="question.imageUrl" alt="Related Image" class="question-image" />
+        <div class="question-text-overlay">
+          {{ question.question }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const UNSPLASH_API_KEY = process.env.VUE_APP_API_KEY_UNSPLASH;
+const RAPID_API_KEY = process.env.VUE_APP_API_KEY_RAPIDAPI;
+
+export default {
+  data() {
+    return {
+      loading: false,
+      question: null,
+    };
+  },
+  methods: {
+    async fetchQuestion() {
+      this.loading = true;
+      try {
+        const response = await axios.get('https://would-you-rather.p.rapidapi.com/wyr/random', {
+          headers: {
+            'X-RapidAPI-Key': RAPID_API_KEY,
+            'X-RapidAPI-Host': 'would-you-rather.p.rapidapi.com',
+          },
+        });
+        const questionData = response.data[0]; // Access the first element in the array
+
+        // Fetch an image related to the question text from Unsplash
+        const imageUrl = await this.fetchRelatedImage(questionData.question);
+
+        // Set the question and image data
+        this.question = { ...questionData, imageUrl };
+        this.loading = false;
+      } catch (error) {
+        console.error(error);
+        this.loading = false;
+      }
+    },
+    async fetchRelatedImage(query) {
+      try {
+        const response = await axios.get(`https://api.unsplash.com/search/photos?query=${query}`, {
+          headers: {
+            'Authorization': `Client-ID ${UNSPLASH_API_KEY}`,
+          },
+        });
+        if (response.data.results.length > 0) {
+          return response.data.results[0].urls.regular; // Use the URL of the first image
+        }
+        return ''; // Return an empty string if no image is found
+      } catch (error) {
+        console.error('Error fetching image:', error);
+        return ''; // Return an empty string on error
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+.site-container {
+  background: linear-gradient(to bottom, #000000, #222222);
+  color: white;
+  padding: 20px;
+  text-align: center;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.btn-show-question {
+  background-color: royalblue;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.btn-show-question:hover {
+  background-color: green;
+}
+
+.loading {
+  font-size: 18px;
+  color: silver;
+  margin: 20px 0;
+}
+
+.question-box {
+  background-color: royalblue;
+  border: 2px solid white;
+  border-radius: 5px;
+  padding: 20px;
+  margin: 20px 0;
+  animation: fadeIn 1s;
+}
+
+.question-image-container {
+  position: relative;
+  overflow: hidden;
+}
+
+.question-image {
+  max-width: 100%;
+}
+
+.question-text-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 24px;
+  background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent background for legibility */
+}
+
+.question-text {
+  color: white;
+  font-size: 24px;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
